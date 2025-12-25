56fa024bce56430b2f9b5ad6064aa7e8e8eb146b
NehaBhagat2621 <nehabhagat2621@gmail.com>
2025-12-25 23:00:27 +0530

    Full Code update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Pages/home'
+import { ToastContainer } from 'react-toastify'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Food.js
@@ -0,0 +1,106 @@
+import image1 from "./assets/image1.jpg";
+import image2 from "./assets/image2.jpg";
+import image3 from "./assets/image3.jpg";
+import image4 from "./assets/image4.jpg";
+import image5 from "./assets/image5.jpg";
+import image6 from "./assets/image6.jpg";
+import image7 from "./assets/image7.jpg";
+import image8 from "./assets/image8.jpg";
+import image9 from "./assets/image9.jpg";
+import image10 from "./assets/image10.jpg";
+import image11 from "./assets/image11.jpg";
+import image12 from "./assets/image12.jpg";
+import image13 from "./assets/image13.jpg";
+import image14 from "./assets/image14.jpg";
+import image15 from "./assets/image15.jpg";
+import image16 from "./assets/image16.jpg";
+import image17 from "./assets/image17.jpg";
+import image18 from "./assets/image18.jpg";
+import image19 from "./assets/image19.jpg";
+import image20 from "./assets/image20.jpg";
+
+
+
+export const food_items = [
+    {
+        "id": 1, "food_name": "Pancakes", "food_category": "breakfast", "food_type": "veg", "food_quantity": 1,
+        "food_image": image1, "price": 499
+    },
+    {
+        "id": 2, "food_name": "Chicken Soup", "food_category": "soups", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image2, "price": 399
+    },
+    {
+        "id": 3, "food_name": "Minestrone Soup", "food_category": "soups", "food_type": "veg", "food_quantity": 1,
+        "food_image": image3, "price": 349
+    },
+    {
+        "id": 4, "food_name": "spaghetti carbonara", "food_category": "pasta", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image4, "price": 999
+    },
+    {
+        "id": 5, "food_name": "Margherita pizza", "food_category": "pizza", "food_type": "veg", "food_quantity": 1,
+        "food_image": image5, "price": 649
+    },
+    {
+        "id": 6, "food_name": "pepperoni pizza ", "food_category": "pizza", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image6, "price": 749
+    },
+    {
+        "id": 7, "food_name": "veggie burger", "food_category": "burger", "food_type": "veg", "food_quantity": 1,
+        "food_image": image7, "price": 349
+    },
+    {
+        "id": 8, "food_name": "chicken burger", "food_category": "burger", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image8, "price": 999
+    },
+    {
+        "id": 9, "food_name": "Tomato soup", "food_category": "soups", "food_type": "veg", "food_quantity": 1,
+        "food_image": image9, "price": 269
+    },
+    {
+        "id": 10, "food_name": "Egg sandwich", "food_category": "breakfast", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image10, "price": 749
+    },
+    {
+        "id": 11, "food_name": "chicken chilli", "food_category": "mai_ncourse", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image11, "price": 999
+    },
+    {
+        "id": 12, "food_name": "mushroom soup", "food_category": "soups", "food_type": "veg", "food_quantity": 1,
+        "food_image": image12, "price": 349
+    },
+    {
+        "id": 13, "food_name": "Hash Browns", "food_category": "breakfast", "food_type": "veg", "food_quantity": 1,
+        "food_image": image13, "price": 249
+    },
+    {
+        "id": 14, "food_name": "Vegetable Soup", "food_category": "soups", "food_type": "veg", "food_quantity": 1,
+        "food_image": image14, "price": 329
+    },
+    {
+        "id": 15, "food_name": "Egg Fried Rice", "food_category":"mai_ncourse", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image15, "price": 599
+    },
+    {
+        "id": 16, "food_name": "Cheese omelette", "food_category": "breakfast", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image16, "price": 299
+    },
+    {
+        "id": 17, "food_name": "Veg Alfredo Pasta", "food_category": "pasta", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image17, "price": 899
+    },
+    {
+        "id": 18, "food_name": "Chicken Alfredo Pasta", "food_category": "pasta", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image18, "price": 1099
+    },
+    {
+        "id": 19, "food_name": "fettuccine alfredo", "food_category": "pasta", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image19, "price": 949
+    },
+    {
+        "id": 20, "food_name": "Hawaiian Pizza", "food_category": "pizza", "food_type": "non_veg", "food_quantity": 1,
+        "food_image": image20, "price": 799
+    },
+]
+
